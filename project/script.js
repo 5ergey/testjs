9e48d138760e33af9cdcb30fe085c67a8fe8da98
SERGEY <akifyev.sergey@gmail.com>
2020-03-30 16:03:49 +0300

    Это наш первый проект

---
+++ project/script.js
@@ -0,0 +1,14 @@
+'use strict';
+//alert("Hello World");
+
+//let answer = confirm("Hello?");
+//console.log(answer);
+
+//let answer = prompt("Есть ли Вам 18?", "Да");
+//console.log(answer);
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
